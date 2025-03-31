d4e71121ceff97a47c3e6e94d7fe813b8e0be9e2
AMAN3-ENG <68887438+AMAN3-ENG@users.noreply.github.com>
2025-03-31 18:09:18 +0000

    com

---
+++ passenger counter/passenger_counter.js
@@ -0,0 +1,21 @@
+// document.getElementById("count-el").innerText = 5
+
+
+let cou
+
+// let firstBatch = 5
+// let secondBatch = 6
+
+
+
+// let count = firstBatch
+
+
+// document.getElementById("count").innerText = count
+
+// console.log(count)
+
+// myAge = 24
+
+// console.log(myAge)
+
