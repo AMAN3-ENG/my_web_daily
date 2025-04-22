520c199c41e2629131e96dcbe3355f557e1b09dd
AMAN3-ENG <68887438+AMAN3-ENG@users.noreply.github.com>
2025-04-22 13:32:38 +0000

    com

--- BlackJack_App/index.js
+++ BlackJack_App/index.js
@@ -1,5 +1,7 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -9,13 +11,32 @@ let message = ""
 
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard() {
+    return 5
+}
+
+function startGame() {
+    // 3. Call renderGame() to render the message
+    renderGame()
+}
+
+
 
 // 2. Create a startGame() function. Move the conditional
 // below (line 11-20) inside the body of the function.
 
-function startGame() {
+function renderGame() {
 
     sumEl.textContent = "Sum: " + sum
+    // render out firstCard and secondCard
+    // cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+
+    for (let index = 0; index < cards.length; index++) {
+        cardsEl.textContent += cards[index] + " "
+        
+    }
     
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
@@ -33,7 +54,18 @@ function startGame() {
     
 }
 
+function newCard() {
+  
+    console.log("Drawing a new card from the deck!")
 
+    let card = getRandomCard()
+
+    sum = sum + card
+    // 4. Call startGame() again to re-render the message
+    renderGame()
+
+
+}
 
 
 
