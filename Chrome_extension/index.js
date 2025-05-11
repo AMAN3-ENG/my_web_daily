485ebdf0c2a747ec5106bb40d3c4e49c21c7b1da
AMAN3-ENG <68887438+AMAN3-ENG@users.noreply.github.com>
2025-05-11 14:16:44 +0000

    fvv

--- Chrome_extension/index.js
+++ Chrome_extension/index.js
@@ -1,4 +1,20 @@
-document.getElementById("input-btn").addEventListener("click",function()
-{
-    console.log("Button clicked!")
-}) 
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems  
+}
